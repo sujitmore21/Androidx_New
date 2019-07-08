2ccf4a6ab427c4c14e68843eb56a21d008e1bec5
Sujit More <sujitmore21@gmail.com>
2019-07-08 15:37:34 +0530

    Storage:private&Public External storage

--- storage/src/main/java/com/example/storage/MainActivity.kt
+++ storage/src/main/java/com/example/storage/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.storage
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,11 +19,17 @@ class MainActivity : AppCompatActivity() {
         val actPrefs = getPreferences(Context.MODE_PRIVATE)
 
         btShRd.setOnClickListener {
-            readFromSharedPrefs(appPrefs)
+            //  readFromSharedPrefs(appPrefs)
+            //  readFromInternalStorage()
+            //  readFromPrivateExternalStorage()
+            readFromPublicExternalStorage()
         }
 
         btShWr.setOnClickListener {
-            writeToSharedPrefs(appPrefs)
+            // writeToSharedPrefs(appPrefs)
+            // writeToInternalStorage()
+            // writeToPrivateExternalStorage()
+            writeToPublicExternalStorage()
         }
     }
 
@@ -45,15 +52,48 @@ class MainActivity : AppCompatActivity() {
         Log.i("@codekul", prefsDt)
     }
 
-    fun writeToInternalStorage() {
+    private fun writeToInternalStorage() {
         val file = File(filesDir, "my.txt")
         Log.i("@codekul", "Path to internal storage ${file.absolutePath}")
         file.bufferedWriter().use { bw -> bw.write("This is my data") }
     }
 
-    fun readToInternalStorage() {
+    private fun readFromInternalStorage() {
         val file = File(filesDir, "my.txt")
         val data = file.readText()
         Log.i("@codekul", "Data available in file is $data")
+    }
+
+    private fun writeToPrivateExternalStorage() {
+        val rootFile = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)
+        val file = File(rootFile, "my.txt")
+        Log.i("@codekul", "Path to private external storage is ${file.absolutePath} ")
+        file.bufferedWriter().use { bw -> bw.write("This data to external storage") }
+    }
+
+    private fun readFromPrivateExternalStorage() {
+        val rootFile = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)
+        val file = File(rootFile, "my.txt")
+
+        val data = file.readText()
+        Log.i("@codekul", "Private External File Data is $data")
+    }
+
+    private fun writeToPublicExternalStorage() {
+        val rootFile = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+
+        val file = File(rootFile, "my.txt")
+        Log.i("@codekul", "Public External Storage Is - ${file.absolutePath}")
+
+        file.bufferedWriter().use { bw -> bw.write("This is external public storage") }
+    }
+
+    private fun readFromPublicExternalStorage() {
+        val rootFile = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+
+        val file = File(rootFile, "my.txt")
+        val data = file.readText()
+        Log.i("@codekul", "External Storage Data is - $data")
+
     }
 }
