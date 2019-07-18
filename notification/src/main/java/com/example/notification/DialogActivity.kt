9fe536c75a6e6df71d96508932e13e83113ff20d
Sujit More <sujitmore21@gmail.com>
2019-07-18 09:15:00 +0530

    retrofit:customDialog

--- notification/src/main/java/com/example/notification/DialogActivity.kt
+++ notification/src/main/java/com/example/notification/DialogActivity.kt
@@ -1,9 +1,11 @@
 package com.example.notification
 
+import android.app.Dialog
 import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_dialog.*
 
@@ -15,25 +17,51 @@ class DialogActivity : AppCompatActivity() {
 
 
         btnShowDialog.setOnClickListener {
-            alertDialog()
+            //alertDialog()
+            customDialog()
         }
     }
 
-    private fun alertDialog(){
+    private fun customDialog() {
 
-        val builder=AlertDialog.Builder(this@DialogActivity)
+        val dialog = Dialog(this@DialogActivity)
+        dialog.setContentView(R.layout.custom_dialog)
+        dialog.setCancelable(false)
+        dialog.setCanceledOnTouchOutside(false)
+        val btnLogout = dialog.findViewById<Button>(R.id.btnDialogLogout)
+        btnLogout.setOnClickListener {
+            startActivity(
+                Intent(this@DialogActivity, DemoActivity::class.java)
+                    .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            )
+            finish()
+        }
+        val btnCancel = dialog.findViewById<Button>(R.id.btnDialogCancel)
+        btnCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+
+
+    private fun alertDialog() {
+
+        val builder = AlertDialog.Builder(this@DialogActivity)
         builder.setMessage("Are you sure to Logout ?")
-        builder.setPositiveButton("Logout",object :DialogInterface.OnClickListener{
+        builder.setPositiveButton("Logout", object : DialogInterface.OnClickListener {
             override fun onClick(dialog: DialogInterface?, which: Int) {
                 dialog!!.dismiss()
-                startActivity(Intent(this@DialogActivity,DemoActivity::class.java)
-                    .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+                startActivity(
+                    Intent(this@DialogActivity, DemoActivity::class.java)
+                        .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                        .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                )
                 finish()
             }
         })
 
-        builder.setNegativeButton("Cancel",object :DialogInterface.OnClickListener{
+        builder.setNegativeButton("Cancel", object : DialogInterface.OnClickListener {
             override fun onClick(dialog: DialogInterface?, which: Int) {
                 dialog!!.dismiss()
             }
@@ -41,11 +69,6 @@ class DialogActivity : AppCompatActivity() {
         builder.create()
         builder.show()
     }
-
-
-
-
-
 
 
 }
